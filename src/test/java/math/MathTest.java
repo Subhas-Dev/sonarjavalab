eb2ee73a948b422cc1c7467e237110ab93ca4882
Subhas-Dev <75505019+Subhas-Dev@users.noreply.github.com>
2021-02-24 14:32:03 -0500

    Update MathTest.java

--- src/test/java/math/MathTest.java
+++ src/test/java/math/MathTest.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.*;
 public class MathTest {
 
   @Test
+  public void testAdd() throws Exception {
+  	assertEquals(Math.add(2, 5), 7);
+  }
+  
+  @Test
   public void testDiv() throws Exception {
   	assertEquals(Math.div(20, 5), 4);
   }
